55d4205197d65cac686ea14826d0f88d35fbaa43
PnIaaL <166519376+PnIaaL@users.noreply.github.com>
2024-04-10 21:21:47 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+import products from './products.js';
+import cart from './cart.js';
+
+let app = document.getElementById('app');
+let temporaryContent = document.getElementById('temporaryContent');
+
+// 加载布局文件
+const loadTemplate = () => {
+    fetch('./template.html')
+    .then(response => response.text())
+    .then(html => {
+        app.innerHTML = html;
+        let contentTab = document.getElementById('contentTab');
+        contentTab.innerHTML = temporaryContent.innerHTML;
+        temporaryContent.innerHTML = null;
+        cart();
+        initApp();
+    })
+}
+loadTemplate();
+const initApp = () => {
+     // 加载商品
+     let listProductHTML = document.querySelector('.listProduct');
+     listProductHTML.innerHTML = null;
+     
+     products.forEach(product => {
+         let newProduct = document.createElement('div');
+         newProduct.classList.add('item');
+         newProduct.innerHTML = 
+         `<a href="./detail.html?id=${product.id}">
+             <img src="${product.image}">
+         </a>
+         <h2>${product.name}</h2>
+         <div class="price">￥${product.price}</div>
+         <button 
+             class="addCart" 
+             data-id='${product.id}'>
+                添加到购物车
+         </button>`;
+         listProductHTML.appendChild(newProduct);
+    });
+}
+
+// 在 index.js 文件中添加以下代码来向页面中添加更多内容
+const appDiv = document.getElementById('app');
+const temporaryContentDiv = document.getElementById('temporaryContent');
+
+// 创建一个新的 div 元素
+const newDiv = document.createElement('div');
+newDiv.textContent = '更多内容';
+
+// 将新的 div 元素添加到页面中
+appDiv.appendChild(newDiv);
